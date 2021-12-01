8b55aabd529bcd46deadba0fb0b4c9c319fc7ecc
Maxmilián Dao <maxmilian.dao@outlook.com>
2021-12-02 00:41:57 +0100

    Add files via upload

---
+++ Nasobky.py
@@ -0,0 +1,6 @@
+x = int(input("Zadejte celé číslo: "))
+if x % 2 == 0 or x % 3 == 0 or x % 5 == 0:
+    print("Číslo je násobkem alespoň jednoho z čísel 2, 3, 5.")
+else:
+    print("Číslo není násobkem žádného z čísel 2, 3, 5.")
+#Omlouvám se za pozdní odevzdání, na úkol jsem zapomněl
